163c6fb7befb126d15a55c7974dfbf85fd90bfa5
Alex <alex@codeship.io>
2017-02-11 12:14:52 -0500

    cleaned up quotes

--- test.sh
+++ test.sh
@@ -6,9 +6,9 @@ TESTSTRING="smarketing" # text you want to check if it exists in the file
 echo "As a user of the WIKI I want to find $TESTSTRING in file $FILE"
 if cat "$FILE" | grep -q "$TESTSTRING"
 then 
-  echo "SUCCESS: $TESTSTRING" found in "$FILE"
+  echo "SUCCESS: $TESTSTRING found in $FILE"
   exit 0; # yes, text exists and exit script with code 0, which means successful
 else
-  echo "FAILURE: $TESTSTRING" not found in "$FILE"
+  echo "FAILURE: $TESTSTRING not found in $FILE"
   exit 1; # no, text does not exist thus exit script with code 1 (or > 0), which means error
 fi
